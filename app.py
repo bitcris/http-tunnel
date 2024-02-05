926c2ff73b39d7ce64bf9621a2b5dee83e94cfb0
Cristiano Ferreira <96497604+bitcris@users.noreply.github.com>
2024-02-05 22:01:43 +0000

    e1

--- app.py
+++ app.py
@@ -1,1 +1,30 @@
-import soocket
+import socket
+
+print('PYTHON CONNECT V1.0\n')
+defaultUrl = 'api.apifree.top'
+
+url = input('URL/IP: ')
+
+if url == "":
+  url = defaultUrl
+
+  
+
+
+def tunnel():
+  print(f'CONECTANDO A [ {url} ]...\n')
+
+# Crie um soquete INET, com fluxo de dados (TCP)
+  s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  s.connect((url, 80))
+
+  print('ORIGEM:',s.getsockname())
+  print('DESTINO:',s.getpeername())
+
+
+
+
+
+
+
+tunnel()
